94f9e2e716c3d06ee9217e29c5804d17d6283b11
fujuhong <fujuhong@joyy.com>
2021-11-26 11:19:44 +0800

    修改配置文件

--- src/main/java/com/qqbot/www/qqBot/envGet.java
+++ src/main/java/com/qqbot/www/qqBot/envGet.java
@@ -3,7 +3,7 @@ package com.qqbot.www.qqBot;
 public class envGet {
     // 配置不同环境的配置文件地址
     private static final String testEnv = "src/main/resources/";
-    private static final String prodEnv = "resources/";
+    private static final String prodEnv = "qqbot/src/main/resources/";
 
     public static String getByEnv(String envName) {
         if (envName.equals("test"))
