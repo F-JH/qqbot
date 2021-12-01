c4fbe2977d440d390322846cb43ccb004a0c0bab
fujuhong <fujuhong@joyy.com>
2021-12-02 00:09:06 +0800

    改一下bug

--- bot-api/src/main/java/com/bot/api/qqBot/envGet.java
+++ bot-api/src/main/java/com/bot/api/qqBot/envGet.java
@@ -3,7 +3,7 @@ package com.bot.api.qqBot;
 public class envGet {
     // 配置不同环境的配置文件地址
     private static final String testEnv = "src/main/resources/";
-    private static final String prodEnv = "qqbot/src/main/resources/";
+    private static final String prodEnv = "qqbot/bot-api/src/main/resources/";
 
     public static String getByEnv(String envName) {
         if (envName.equals("test"))
