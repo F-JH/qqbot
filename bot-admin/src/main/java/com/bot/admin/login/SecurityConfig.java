44cd65cf8bfe6812ea39fd925e64beb611aff46c
fjh <gaodaziyou@126.com>
2021-12-10 02:30:24 +0800

    ...

--- bot-admin/src/main/java/com/bot/admin/login/SecurityConfig.java
+++ bot-admin/src/main/java/com/bot/admin/login/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.bot.admin.login;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -28,15 +27,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/js/**", "/css/**","/images/**");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-//                .loginPage("/login.html")
-                .permitAll()
-                .and()
-                .csrf().disable();
-    }
 }
